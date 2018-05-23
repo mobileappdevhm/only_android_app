01fea5d9ef0e59548d18c5adc0762148365c3f98
Felix Peither <felix@peither.de>
2018-05-23 07:33:20 +0200

    bottom nav bar with no function yet

--- app/src/main/java/felix/peither/de/cie_for_android/HomePage.java
+++ app/src/main/java/felix/peither/de/cie_for_android/HomePage.java
@@ -1,13 +1,27 @@
 package felix.peither.de.cie_for_android;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HomePage extends AppCompatActivity {
+
+    BottomNavigationView nav_bar;
+    CoordinatorLayout cl;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        cl = (CoordinatorLayout) findViewById(R.id.home_layout);
+        nav_bar = (BottomNavigationView) findViewById(R.id.nav_bar);
+
+        toolbar = (Toolbar) findViewById(R.id.courses_toolbar);
+        toolbar.setTitle("Courses");
+        setSupportActionBar(toolbar);
     }
 }
