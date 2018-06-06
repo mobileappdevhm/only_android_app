4614f20d866902867d19261603af59aec5c841fe
Felix Peither <felix@peither.de>
2018-06-06 17:58:51 +0200

    toolbar with skip button in login page

--- app/src/main/java/felix/peither/de/cie_for_android/LoginActivity.java
+++ app/src/main/java/felix/peither/de/cie_for_android/LoginActivity.java
@@ -1,15 +1,28 @@
 package felix.peither.de.cie_for_android;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    MenuItem skipText;
+    LinearLayout linearLayout;
+    ScrollView scrollView;
     private EditText Name;
     private EditText Password;
     private TextView Info;
@@ -26,6 +39,13 @@ public class LoginActivity extends AppCompatActivity {
         Info = (TextView) findViewById(R.id.tvInfo);
         Login = (Button) findViewById(R.id.btnLogin);
 
+        skipText = (MenuItem) findViewById(R.id.skip_login);
+
+        toolbar = (Toolbar) findViewById(R.id.log_in_toolbar);
+        setSupportActionBar(toolbar);
+
+        //toolbar.setTitle(R.string.toolbar_title);
+
         Info.setText("Nº of attempts remaining: 5");
 
         Login.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +56,20 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.skip_login){
+            startActivity(new Intent(this, SecondActivity.class));
+        }
+
+        return true;
+    }
 
     private void validate (String userName, String userPassword){
         if((userName.equals("")) && (userPassword.equals(""))) {
